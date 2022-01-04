fb107f16203a8bda0198dc0c374992052034930f
Gerrit Nowald <gerrit.nowald@continental-corporation.com>
2022-01-04 16:29:54 +0100

    added Jeffcott rotor

--- rotor_ode.py
+++ rotor_ode.py
@@ -18,37 +18,67 @@ g = 9.81    # gravitational acceleration
 # parameters
 
 m = 0.1         # mass of rotor / kg
-epsilonR = 5e-6	# unbalance radius / m
-c = 1e4         # bearing stiffness / N/m
+epsR = 5e-6     # unbalance radius / m
 
-tmax = 2        # max. time of calculation / s
+mj = 1e-2       # journal mass / kg
+cs = 1e5        # shaft stiffness / N/m
 
-fmax = 1.5*np.sqrt(c/m)/2/np.pi     # max rotational frequency / Hz (based on natural frequency)
+cb = 1e4        # bearing stiffness / N/m
+
+tmax = 5        # max. time of calculation / s
+fmax = 1.5*np.sqrt(cb/m)/2/np.pi     # max rotational frequency / Hz (based on natural frequency)
 
 # -----------------------------------------------------------------------------
 # functions
 
-def bearing(x,y,c):
-    # linear elastic bearing
-    return - c*x, - c*y # horizontal, vertical bearing force
-
-def ODE(t, q, m, epsilonR, c, arot, g):
-    # ode in state space formulation
-    x, y, xd, yd = q        # states
-    Fx, Fy = bearing(x,y,c) # bearing forces
-    return [ xd, yd,
-        Fx/m + epsilonR*( arot*np.sin(0.5*arot*t**2) + (arot*t)**2*np.cos(0.5*arot*t**2)) ,
-        Fy/m + epsilonR*(-arot*np.cos(0.5*arot*t**2) + (arot*t)**2*np.sin(0.5*arot*t**2)) - g
+def bearing_lin_elast(x,y,cb):
+    return [    # horizontal, vertical bearing force
+        - cb*x,
+        - cb*y
         ]
+
+def unbalance_const_acc(epsR,arot,t):
+    return [    # horizontal, vertical unbalance force
+        epsR*( arot*np.sin(0.5*arot*t**2) + (arot*t)**2*np.cos(0.5*arot*t**2)) ,
+        epsR*(-arot*np.cos(0.5*arot*t**2) + (arot*t)**2*np.sin(0.5*arot*t**2))
+        ]
+
+def rotor_rigid(t, q, m, epsR, cb, d, arot, g):
+    x, y, xd, yd = q                        # states
+    FB = bearing_lin_elast(x,y,cb)          # bearing forces
+    FU = unbalance_const_acc(epsR,arot,t)   # unbalance forces
+    return [ xd, yd,                        # ode in state space formulation
+            FB[0]/m + FU[0] - d*xd ,
+            FB[1]/m + FU[1] - d*yd - g
+            ]
+
+def rotor_Jeffcott(t, q, m, epsR, mj, cs, cb, d, arot, g):
+    xm, ym, xj, yj, xdm, ydm, xdj, ydj = q  # states
+    FB = bearing_lin_elast(xj,yj,cb)        # bearing forces
+    FU = unbalance_const_acc(epsR,arot,t)   # unbalance forces
+    return [ xdm, ydm, xdj, ydj,            # ode in state space formulation
+            - d*(xdm-xdj)/m  - cs*(xm-xj)/m  - d*xdm + FU[0]     ,
+            - d*(ydm-ydj)/m  - cs*(ym-yj)/m  - d*ydm + FU[1] - g , 
+            - d*(xdj-xdm)/mj - cs*(xj-xm)/mj - d*xdj + FB[0]/mj  ,
+            - d*(ydj-ydm)/mj - cs*(yj-ym)/mj - d*ydj + FB[1]/mj -g
+            ]
 
 # -----------------------------------------------------------------------------
 # calculation
 
-q0   = [0, -m*g/c, 0, 0]    # initial conditions [displ. x, displ. y, speed x, speed y] (static equilibrium)
 arot = 2*np.pi*fmax/tmax    # acceleration of rotor speed / rad/s**2 (reach fmax in tmax)
+d    = 1e-2*np.sqrt(cs/m)   # damping coefficient / Ns/m
 
-# numerical integration
-res = solve_ivp(ODE, [0, tmax], q0, args=(m, epsilonR, c, arot, g),
+# rigid rotor
+# q0   = [0, -m*g/cb, 0, 0]    # initial conditions [displ. x, displ. y, speed x, speed y] (static equilibrium)
+# res = solve_ivp(rotor_rigid, [0, tmax], q0, args=(m, epsR, cb, d, arot, g),
+#                 t_eval = np.linspace(0, tmax, int(tmax*fmax*30) ),    # points of orbit at highest frequency
+#                 rtol=1e-6, atol=1e-6 )
+
+# Jeffcott rotor
+y0 = - (m+mj)*g/cb
+q0  = [0, y0, 0, y0, 0, 0, 0, 0]    # initial conditions [displ. x, displ. y, speed x, speed y] (static equilibrium)
+res = solve_ivp(rotor_Jeffcott, [0, tmax], q0, args=(m, epsR, mj, cs, cb, d, arot, g),
                 t_eval = np.linspace(0, tmax, int(tmax*fmax*30) ),    # points of orbit at highest frequency
                 rtol=1e-6, atol=1e-6 )
 
