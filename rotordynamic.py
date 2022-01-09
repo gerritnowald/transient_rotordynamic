490eaa022443404ec5b911adfe0e6eaaf704f66b
Gerrit Nowald <gerrit.nowald@gmail.com>
2022-01-09 08:40:51 +0100

    atan replaced with atan2

--- rotordynamic.py
+++ rotordynamic.py
@@ -50,10 +50,10 @@ def short_bearing_forces(eps,epsS,phiS):
         cos_alpha, sin_alpha = cos_sin(np.array([2*epsS, eps*(1-2*phiS)]))
         cos_alpha, sin_alpha = - sin_alpha, - cos_alpha     # needs to be corrected
         # integrals
-        piS = (sin_alpha < 0)*np.pi
+        angle = np.arctan2(np.sqrt(1-eps**2),eps*sin_alpha)
         I1 = 2*eps*cos_alpha**3/(1-eps**2*cos_alpha**2)**2
-        I2 = - eps*sin_alpha*(1-(2-eps**2)*cos_alpha**2)/(1-eps**2)/(1-eps**2*cos_alpha**2)**2 + (piS + np.arctan(np.sqrt(1-eps**2)/eps/sin_alpha))/(1-eps**2)**1.5
-        I3 = - eps*sin_alpha*(4-eps**2*(1+(2+eps**2)*cos_alpha**2))/(1-eps**2)**2/(1-eps**2*cos_alpha**2)**2 + (piS + np.arctan(np.sqrt(1-eps**2)/eps/sin_alpha))*(2+eps**2)/(1-eps**2)**2.5
+        I2 = - eps*sin_alpha*(1-(2-eps**2)*cos_alpha**2)/(1-eps**2)/(1-eps**2*cos_alpha**2)**2 + angle/(1-eps**2)**1.5
+        I3 = - eps*sin_alpha*(4-eps**2*(1+(2+eps**2)*cos_alpha**2))/(1-eps**2)**2/(1-eps**2*cos_alpha**2)**2 + angle/(1-eps**2)**2.5*(2+eps**2)
         # dimensionless forces
         fr   = I2*eps*(1-2*phiS) - 2*epsS*I1
         fphi = I1*eps*(1-2*phiS) + 2*epsS*(I2-I3)
