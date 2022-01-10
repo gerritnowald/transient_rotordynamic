cf9c039c89750c63d7e6764f5103cf2d5add95eb
Gerrit Nowald <gerrit.nowald@gmail.com>
2022-01-10 11:39:34 +0100

    test

--- rotordynamic.py
+++ rotordynamic.py
@@ -50,7 +50,8 @@ def short_bearing_forces(eps,epsS,phiS):
         cos_alpha, sin_alpha = cos_sin(np.array([2*epsS, eps*(1-2*phiS)]))
         cos_alpha, sin_alpha = - sin_alpha, - cos_alpha     # needs to be corrected
         # integrals
-        angle = np.arctan2(np.sqrt(1-eps**2),eps*sin_alpha)
+        angle = np.arctan2(np.sqrt(1-eps**2),eps*sin_alpha)     # old correct version
+        # angle = np.arctan2(np.sqrt(1-eps**2),-eps*cos_alpha)
         I1 = 2*eps*cos_alpha**3/(1-eps**2*cos_alpha**2)**2
         I2 = - eps*sin_alpha*(1-(2-eps**2)*cos_alpha**2)/(1-eps**2)/(1-eps**2*cos_alpha**2)**2 + angle/(1-eps**2)**1.5
         I3 = - eps*sin_alpha*(4-eps**2*(1+(2+eps**2)*cos_alpha**2))/(1-eps**2)**2/(1-eps**2*cos_alpha**2)**2 + angle/(1-eps**2)**2.5*(2+eps**2)
@@ -62,7 +63,7 @@ def short_bearing_forces(eps,epsS,phiS):
         # fphi	-->	-fr
     return np.array([fr, fphi])
 
-# print(short_bearing_forces(0.7,1,2) )
+# print(short_bearing_forces(0.7,5,2) )
 
 def bearing_journal_short(q,B,D,C,eta,omega0):
     # state vector q = [x, y, xd, yd]
