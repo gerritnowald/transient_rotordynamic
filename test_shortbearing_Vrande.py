a531b29599d2e6721dc0a9a6d7e37267f26ad8ee
Gerrit Nowald <gerrit.nowald@gmail.com>
2022-01-10 17:19:13 +0100

    updated readme, changed variable names

--- test_shortbearing_Vrande.py
+++ test_shortbearing_Vrande.py
@@ -7,24 +7,22 @@ Created on Mon Jan 10 15:33:04 2022
 
 import numpy as np
 
-def cos_sin(q):                       # q = [x,y]
-    return q/np.sqrt(np.sum(q**2))    # [cos, sin]
-
 def short_bearing_forces(eps,epsS,phiS):
 # journal bearing forces short bearing theory
-    vs = np.sqrt(epsilonS**2+(epsilon*(phiS-0.5))**2)
-    alpha = np.arctan2(-epsilon*(phiS-0.5), epsilonS)
+    vs = np.sqrt(epsilonS**2+(eps*(phiS-0.5))**2)
+    # alpha = np.arctan2(-eps*(phiS-0.5), epsS)
+    alpha = np.arctan2(eps*(1-2*phiS), 2*epsS)
     if np.cos(alpha)>=0:
         delta = 1
     elif np.cos(alpha)<0:
         delta = -1
-    A = (epsilon+np.sin(alpha))/(1+epsilon*np.sin(alpha))
-    B = (epsilon-np.sin(alpha))/(1-epsilon*np.sin(alpha))
+    A = (eps+np.sin(alpha))/(1+eps*np.sin(alpha))
+    B = (eps-np.sin(alpha))/(1-eps*np.sin(alpha))
     # integrals
-    I001 = (np.arccos(-delta*A)+np.arccos(-delta*B))/np.sqrt(1-epsilon**2)
-    I023 = 1/(2*(1-epsilon**2)**2)*((1+2*epsilon**2)*I001+2*epsilon*np.cos(alpha)*(3+(2-5*epsilon**2)*(np.sin(alpha))**2)/(1-epsilon**2*(np.sin(alpha))**2)**2)
-    I113 = -2*epsilon*(np.sin(alpha))**3/(1-epsilon**2*(np.sin(alpha))**2)**2
-    I203 = 1/(2*(1-epsilon**2))*(I001+2*epsilon*np.cos(alpha)*(1-(2-epsilon**2)*(np.sin(alpha))**2)/(1-epsilon**2*(np.sin(alpha))**2)**2 )
+    I001 = (np.arccos(-delta*A)+np.arccos(-delta*B))/np.sqrt(1-eps**2)
+    I023 = 1/(2*(1-eps**2)**2)*((1+2*eps**2)*I001+2*eps*np.cos(alpha)*(3+(2-5*eps**2)*(np.sin(alpha))**2)/(1-eps**2*(np.sin(alpha))**2)**2)
+    I113 = -2*eps*(np.sin(alpha))**3/(1-eps**2*(np.sin(alpha))**2)**2
+    I203 = 1/(2*(1-eps**2))*(I001+2*eps*np.cos(alpha)*(1-(2-eps**2)*(np.sin(alpha))**2)/(1-eps**2*(np.sin(alpha))**2)**2 )
     # Impedanz-Komponenten (Kurzlager)
     Wr   = 2*(I023*np.cos(alpha)-I113*np.sin(alpha))
     Wphi = 2*(I113*np.cos(alpha)-I203*np.sin(alpha))
